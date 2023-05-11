ddda10bb8ee045ea3898d11865dbb4b7d24ea781
Rahul Das <91859535+RahulDasKumar@users.noreply.github.com>
2023-05-10 22:28:44 -0400

    Changed some features

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,7 +3,7 @@ const { clientId, guildId, token } = require('./config.json');
 const fs = require('node:fs');
 
 const commands = [];
-// Grab all the command files from the commands directory you created earlier
+// Grabs all the js files from the command directory
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
@@ -15,12 +15,12 @@ for (const file of commandFiles) {
 // Construct and prepare an instance of the REST module
 const rest = new REST({ version: '10' }).setToken(token);
 
-// and deploy your commands!
+
 (async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-		// The put method is used to fully refresh all commands in the guild with the current set
+		//puts all the commands in the route so user can see them
 		const data = await rest.put(
 			Routes.applicationGuildCommands(clientId, guildId),
 			{ body: commands },
